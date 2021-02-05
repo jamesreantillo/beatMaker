775e5b3a4a1e956631c66008fdf1ae01c7906963
james reantillo <james.reantillo@gmail.com>
2021-02-05 00:40:10 -0800

    add mute

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ class Drumkit {
     this.bpm = 150;
     this.isPlaying = null;
     this.selects = document.querySelectorAll('select');
+    this.muteBtns = document.querySelectorAll('.mute');
   }
 
   activePad() {
@@ -84,6 +85,36 @@ class Drumkit {
         break;
     }
   }
+
+  mute(e) {
+    const muteIndex = e.target.getAttribute('data-track');
+    e.target.classList.toggle('active');
+    if (e.target.classList.contains('active')) {
+      switch (muteIndex) {
+        case '0':
+          this.kickAudio.volume = 0;
+          break;
+        case '1':
+          this.snareAudio.volume = 0;
+          break;
+        case '2':
+          this.hihatAudio.volume = 0;
+          break;
+      }
+    } else {
+      switch (muteIndex) {
+        case '0':
+          this.kickAudio.volume = 1;
+          break;
+        case '1':
+          this.snareAudio.volume = 1;
+          break;
+        case '2':
+          this.hihatAudio.volume = 1;
+          break;
+      }
+    }
+  }
 }
 
 const drumkit = new Drumkit();
@@ -105,5 +136,11 @@ drumkit.playBtn.addEventListener('click', () => {
 drumkit.selects.forEach((select) => {
   select.addEventListener('change', function (e) {
     drumkit.changeSound(e);
+  });
+});
+
+drumkit.muteBtns.forEach((btn) => {
+  btn.addEventListener('click', function (e) {
+    drumkit.mute(e);
   });
 });
